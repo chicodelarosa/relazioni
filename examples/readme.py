38ff55ed0989d6a98242e4d5d2b3c4caca53b66f
Dan A. Rosa De Jesus <dan.rosadejesus@pnnl.gov>
2022-04-11 16:00:25 -0700

    Adding example

---
+++ examples/readme.py
@@ -0,0 +1,12 @@
+import numpy as np
+from relationships import associations
+
+v1, v2 = np.array([1, 1, 2]), np.array([1, 1, 2])
+
+matth_corr = associations.matthews_corr(v1, v2)
+print(matth_corr) # 1.0
+
+v1, v2 = np.array([1, 1, 2]), np.array([2, 1, 2])
+
+matth_corr = associations.matthews_corr(v1, v2)
+print(matth_corr) # 0.5
